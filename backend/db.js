0ec24ee07ee86d710db2006fe782526d3195e496
eva1606 <168004475+eva1606@users.noreply.github.com>
2025-07-17 11:29:35 +0300

    Configure PostgreSQL connection with dotenv

---
+++ backend/db.js
@@ -0,0 +1,13 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
+console.log("✅ DATABASE_URL =", process.env.DATABASE_URL);
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+module.exports = pool;
