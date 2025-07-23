d4bd836837885f6d0898d41da19e554998591c1e
shyshy3010 <journo.shyrel@gmail.com>
2025-07-23 14:57:59 +0300

    modification in server for routes

--- backend/server.js
+++ backend/server.js
@@ -7,15 +7,14 @@ app.use(express.json());
 
 const userRoutes = require('./routes/users');
 const propertiesRoutes = require('./routes/properties');
-const profilRoutes = require('./routes/profilRoutes');
-// IMPORTATION DE TA ROUTE
+const profileRoutes = require('./routes/profilRoutes');
 const potentialRoomatesRoutes = require('./routes/potentialRoomatesRoutes');
 
 // UTILISATION
 app.use('/api/potential-roommates', potentialRoomatesRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/properties', propertiesRoutes);
-app.use('/api/profil_users', profilRoutes);
+app.use('/api/profil_users', profileRoutes);
 
 
 
