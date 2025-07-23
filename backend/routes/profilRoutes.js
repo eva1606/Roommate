08b91121487263f11438eebe7194ce7a4558e4b9
shyshy3010 <journo.shyrel@gmail.com>
2025-07-23 21:54:04 +0300

    new routes editprofil for upload photo

--- backend/routes/profilRoutes.js
+++ backend/routes/profilRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const profileController = require('../controllers/profileController');
-router.get('/:id', profileController.getProfileById);
-router.put('/:id', profileController.updateProfileById);
+const { getProfileById, updateProfileById } = require('../controllers/profilController');
+
+router.get('/:id', getProfileById);
+router.put('/:id', upload.single('photo'), updateProfileById); // ✅ upload photo
+
 
 module.exports = router;
