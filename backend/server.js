8b651c859da18b5d05e51d18e58994e2dbd4cc2b
shyshy3010 <journo.shyrel@gmail.com>
2025-07-23 21:20:07 +0300

    routes register user adjusted

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const path = require('path');
 
 app.use(cors()); 
 app.use(express.json());
@@ -8,14 +9,14 @@ app.use(express.json());
 const userRoutes = require('./routes/users');
 const propertiesRoutes = require('./routes/properties');
 const profileRoutes = require('./routes/profilRoutes');
-const { getCompatibleRoommates } = require('../controllers/potentialRoomatesController');
+const potentialRoommatesRoutes = require('./routes/potentialRoomatesRoutes');
 
-router.get('/:id', getCompatibleRoommates); 
+app.use('/api/potential-roommates', potentialRoommatesRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/properties', propertiesRoutes);
 app.use('/api/profil_users', profileRoutes);
-
-
+// 🔓 Pour permettre l'accès aux photos uploadées
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 const PORT = process.env.PORT || 5050;
 const os = require("os");
