6ee26b12cf6ce048bb442871e4640752c6703ba0
shyshy3010 <journo.shyrel@gmail.com>
2025-07-24 13:44:05 +0300

    new routes for fetch available controller

--- backend/routes/propertiesavailable.js
+++ backend/routes/propertiesavailable.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const propertiesAvailableController = require('../controllers/propertiesavailableController');
 
-router.get('/', propertiesAvailableController.getAvailableProperties);
+const { getFilteredApartments } = require('../controllers/propertiesavailableController');
+
+// Route pour récupérer les appartements filtrés selon le profil utilisateur
+router.get('/filtered/:id', getFilteredApartments);
 
 module.exports = router;
