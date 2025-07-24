f1abbee95ae379491c6aa1f4de99c951f57d5974
shyshy3010 <journo.shyrel@gmail.com>
2025-07-24 17:13:32 +0300

    new routes for put and delete

--- backend/routes/propertiesavailable.js
+++ backend/routes/propertiesavailable.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getFilteredApartments, saveAvailableApartments } = require('../controllers/propertiesavailableController');
+const { getFilteredApartments, saveAvailableApartments,addToFavorites,deleteValidApartment } = require('../controllers/propertiesavailableController');
 
 router.get('/filtered/:id', getFilteredApartments);
 router.put('/save/:id', saveAvailableApartments); // 👈 Nouvelle route PUT
+router.post('/favorites', addToFavorites);
+router.delete('/apartment-valid/:id', deleteValidApartment);
 
 module.exports = router;
