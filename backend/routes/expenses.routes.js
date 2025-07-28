526f78925e44c369ddb7f9d823f2df758552afe1
shyshy3010 <journo.shyrel@gmail.com>
2025-07-28 20:13:47 +0300

    new routes

--- backend/routes/expenses.routes.js
+++ backend/routes/expenses.routes.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 const expensesController = require("../controllers/expenses.controller");
 
-router.get("/property/:userId", expensesController.getExpensesByUser);
+// GET: Toutes les dépenses liées à la propriété louée par un user
+router.get("/property/:userId", expensesController.getExpensesForUserProperty);
+
+// POST: Ajouter une dépense
 router.post("/", expensesController.addExpense);
 
 module.exports = router;
