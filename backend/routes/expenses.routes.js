f43534a5ee73558f70d4038fb1fb55dabb7b4e36
shyshy3010 <journo.shyrel@gmail.com>
2025-07-31 12:21:18 +0300

    delete com

--- backend/routes/expenses.routes.js
+++ backend/routes/expenses.routes.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const expensesController = require("../controllers/expenses.controller");
 
-// GET: Toutes les dépenses liées à la propriété louée par un user
+
 router.get("/property/:userId", expensesController.getExpensesForUserProperty);
 
-// POST: Ajouter une dépense
+
 router.post("/", expensesController.addExpense);
 
 module.exports = router;
